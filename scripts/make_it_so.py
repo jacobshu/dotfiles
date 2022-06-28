30e704d13e6a56be30c9318c844fae6cbed79eea
Jacob Shulenberger <jacob@builtdigital.io>
2022-06-28 06:51:40 -0500

    feat: add hash check for sql, fix env loading

--- scripts/make_it_so.py
+++ scripts/make_it_so.py
@@ -1,36 +1,41 @@
-# from dotenv import load_env
-from pyngrok import ngrok
 import os
+import sys
+import hashlib
+import fileinput
+from pyngrok import ngrok
+from dotenv import main
 
-# load_dotenv()
+main.load_dotenv()
+
 # pull changes from remote
-
-# Open Docker and start the daemon
-# Run the swm container
+api_repo = os.getenv("GIT_API")
+app_repo = os.getenv("GIT_APP")
 
 # Check for a change in the stored procedures and prompt
 # Since there's no ARM support for sqlcmd the procedure query has to be run on a GUI client
+with open(os.getenv("SQL_PATH"), 'rb') as f:
+    new_hash = hashlib.md5()
+    for chunk in iter(lambda: f.read(4096), b""):
+        new_hash.update(chunk)
+    if os.getenv("SQL_HASH") != new_hash.hexdigest():
+        sys.exit("SQL hash difference.\nNew hash: " + new_hash.hexdigest())
+
+# Open Docker and start the daemon
+# Run the swm container
 
 # Open an HTTPS tunnel on port 5003 with ngrok
 http_tunnel = ngrok.connect("5003", bind_tls=True)
 
 # Get the URL from ngrok without the protocol
 public_url = http_tunnel.public_url[8:]
-pkg_env = "~/dev/pkg-mgmt/assets/.env"
-
-with open(pkg_env, 'r+') as file:
-    # read a list of lines into data
-    lines = file.readlines()
-    new_lines = []
-    for line in lines:
-        new_lines.append(line)
-        if "SERVER" in line:
-            print('found: ', line)
-            new_lines.append("SERVER="+public_url)
-    file.writelines(new_lines)
 
 # Write the public ngrok URL to the pkg-mgmt .env file
-
+pkg_env = os.getenv("PKG_ENV")
+for line in fileinput.input(pkg_env, inplace=True):
+    if "SERVER" in line:
+        sys.stdout.write("SERVER=" + public_url + "\n")
+    else:
+        sys.stdout.write(line)
 
 # Start the API
 # Start the app
