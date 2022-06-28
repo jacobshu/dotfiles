c6a1622d245f81f92719e934e5644e9a2f75923a
Jacob Shulenberger <jacob@builtdigital.io>
2022-06-27 20:59:32 -0500

    feat: add basic reading of .env file

--- scripts/make_it_so.py
+++ scripts/make_it_so.py
@@ -1,7 +1,8 @@
-from dotenv import load_env
+# from dotenv import load_env
 from pyngrok import ngrok
+import os
 
-load_dotenv()
+# load_dotenv()
 # pull changes from remote
 
 # Open Docker and start the daemon
@@ -15,6 +16,18 @@ http_tunnel = ngrok.connect("5003", bind_tls=True)
 
 # Get the URL from ngrok without the protocol
 public_url = http_tunnel.public_url[8:]
+pkg_env = "~/dev/pkg-mgmt/assets/.env"
+
+with open(pkg_env, 'r+') as file:
+    # read a list of lines into data
+    lines = file.readlines()
+    new_lines = []
+    for line in lines:
+        new_lines.append(line)
+        if "SERVER" in line:
+            print('found: ', line)
+            new_lines.append("SERVER="+public_url)
+    file.writelines(new_lines)
 
 # Write the public ngrok URL to the pkg-mgmt .env file
 
