3070a886c6b0d8934ad1c989d6b8c7e5193ecee5
Jacob Shulenberger <jacob@builtdigital.io>
2023-07-13 11:26:24 -0500

    refactor: prefer hex literal colors

--- nvim/after/plugin/nightfox.lua
+++ nvim/after/plugin/nightfox.lua
@@ -4,17 +4,17 @@ require("nightfox").setup({
   palettes = {
     -- Forestfox overrides
     nightfox = {
-      black   = Shade.new("#373c44", 0.15, -0.15),
-      red     = Shade.new("#d86575", 0.15, -0.15),
-      green   = Shade.new("#86b981", 0.10, -0.15),
-      yellow  = Shade.new("#dbbe7a", 0.15, -0.15),
-      blue    = Shade.new("#79b7c9", 0.15, -0.15),
-      magenta = Shade.new("#b189d6", 0.30, -0.15),
-      cyan    = Shade.new("#74c8a9", 0.15, -0.15),
-      white   = Shade.new("#d9d5c4", 0.15, -0.15),
-      orange  = Shade.new("#ed9c6b", 0.15, -0.15),
-      pink    = Shade.new("#d68ac0", 0.15, -0.15),
-     
+      black =   Shade.new("#373c44", "#555960", "#2f333a"),
+      red =     Shade.new("#d86575", "#de7c8a", "#b85663"),
+      green =   Shade.new("#86b981", "#92c08e", "#729d6e"),
+      yellow =  Shade.new("#dbbe7a", "#e0c88e", "#baa268"),
+      blue =    Shade.new("#79b7c9", "#8dc2d1", "#679cab"),
+      magenta = Shade.new("#b189d6", "#c8ace2", "#9674b6"),
+      cyan =    Shade.new("#74c8a9", "#89d0b6", "#63aa90"),
+      white =   Shade.new("#d9d5c4", "#dfdbcd", "#b8b5a7"),
+      orange =  Shade.new("#ed9c6b", "#f0ab81", "#c9855b"),
+      pink =    Shade.new("#d68ac0", "#dc9cc9", "#b675a3"),
+      
       comment = "#7c9290",
 
       bg0     = "#1a2229", -- Dark bg (status line and float)
