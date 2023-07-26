0f9f365c5484ef71ed9f404a549bddab3a4f657e
Jacob Shulenberger <jacob@builtdigital.io>
2023-07-26 15:09:37 -0500

    feat: add defaults, cleanup readme

--- README.md
+++ README.md
@@ -7,8 +7,3 @@ A repository to host various configuration files for future use.
 This is in flux currently. The target system will be to have a properly versioned Rust CLI that can be downloaded and contains all the code necessary to bootstrap a system.
 
 
-## Templating 
-
-This repository is indebted to a number of resources including:
-- [Rosco Kalis's dotfiles](https://github.com/rkalis/dotfiles) covered in [this blog post](https://kalis.me/dotfiles-automating-macos-system-configuration/). 
-
--- macos/set-defaults.sh
+++ macos/set-defaults.sh
@@ -1,7 +1,5 @@
 # Sets reasonable macOS defaults.
 #
-# Or, in other words, set shit how I like in macOS.
-#
 # The original idea (and a couple settings) were grabbed from:
 #   https://github.com/mathiasbynens/dotfiles/blob/master/.macos
 #
@@ -9,6 +7,9 @@
 
 # Switch to dark mode
 osascript -e 'tell application "System Events" to tell appearance preferences to set dark mode to 1'
+
+# Set clock format
+defaults write com.apple.menuextra.clock "DateFormat" -string "\"EEE d MMM HH:mm:ss\"" 
 
 # Disable press-and-hold for keys in favor of key repeat.
 defaults write -g ApplePressAndHoldEnabled -bool false
@@ -32,6 +33,38 @@ defaults write NSGlobalDomain KeyRepeat -int 1
 defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -bool true
 defaults write com.apple.finder ShowRemovableMediaOnDesktop -bool true
 
+# Finder: show file extensions
+defaults write NSGlobalDomain "AppleShowAllExtensions" -bool "true"
+
+# Finder: sort by type, folder then file
+defaults write com.apple.finder "_FXSortFoldersFirst" -bool "true"
+defaults write com.apple.finder "_FXSortFoldersFirstOnDesktop" -bool "true"
+
+# Finder: clear old items in trash
+defaults write com.apple.finder "FXRemoveOldTrashItems" -bool "true"
+
+# Finder: disable warning for file extension changes
+defaults write com.apple.finder "FXEnableExtensionChangeWarning" -bool "false"
+
+
+# Always show scrollbars
+defaults write NSGlobalDomain AppleShowScrollBars -string "Always"
+# Possible values: `WhenScrolling`, `Automatic` and `Always`
+
+# Expand save panel by default
+defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode -bool true
+defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode2 -bool true
+
+# Expand print panel by default
+defaults write NSGlobalDomain PMPrintingExpandedStateForPrint -bool true
+defaults write NSGlobalDomain PMPrintingExpandedStateForPrint2 -bool true
+
+# Save to disk (not to iCloud) by default
+defaults write NSGlobalDomain NSDocumentSaveNewDocumentsToCloud -bool false
+
+# Disable the “Are you sure you want to open this application?” dialog
+defaults write com.apple.LaunchServices LSQuarantine -bool false
+
 # Hide recent apps from Dock
 defaults write com.apple.dock show-recents -bool false
 
@@ -39,17 +72,12 @@ defaults write com.apple.dock show-recents -bool false
 defaults write com.apple.screencapture disable-shadow -bool true
 
 # Create and set screenshot location
-mkdir -p ~/Screenshots
-defaults write com.apple.screencapture location ~/Screenshots
+mkdir -p ~/Desktop/screenshots
+defaults write com.apple.screencapture location ~/Desktop/screenshots
 
 # Run the screensaver if we're in the bottom-left hot corner.
 defaults write com.apple.dock wvous-bl-corner -int 5
 defaults write com.apple.dock wvous-bl-modifier -int 0
-
-# Configure iTerm to use preferences in dotfiles directory
-defaults write com.googlecode.iterm2 LoadPrefsFromCustomFolder -bool true
-defaults write com.googlecode.iterm2 PrefsCustomFolder -string "$HOME/dev/dotfiles/config/iterm"
-defaults write com.googlecode.iterm2 NoSyncNeverRemindPrefsChangesLostForFile_selection -bool false
 
 # Hide Safari's bookmark bar.
 defaults write com.apple.Safari ShowFavoritesBar -bool false
@@ -59,4 +87,5 @@ defaults write com.apple.Safari IncludeInternalDebugMenu -bool true
 defaults write com.apple.Safari IncludeDevelopMenu -bool true
 defaults write com.apple.Safari WebKitDeveloperExtrasEnabledPreferenceKey -bool true
 defaults write com.apple.Safari "com.apple.Safari.ContentPageGroupIdentifier.WebKit2DeveloperExtrasEnabled" -bool true
-defaults write NSGlobalDomain WebKitDeveloperExtras -bool true
+defaults write NSGlobalDomain WebKitDeveloperExtras -bool true 
+killall Finder
