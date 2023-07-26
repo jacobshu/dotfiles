7a65f293a9621016f4f653a0a328cbf40ee598cd
Jacob Shulenberger <jacob@builtdigital.io>
2023-07-26 15:09:02 -0500

    chore: cleanup

--- scripts/generatePaletteFiles.js
+++
@@ -1,9 +0,0 @@
-import fs from 'fs';
-import { fileURLToPath } from 'node:url';
-import { dirname, join } from 'node:path';
-
-const __dirname = dirname(fileURLToPath(import.meta.url));
-const paletteFile = join(__dirname, "..", "/config/synthwavePalette.json");
-
-const palette = JSON.parse(fs.readFileSync(paletteFile));
-console.log(palette);
