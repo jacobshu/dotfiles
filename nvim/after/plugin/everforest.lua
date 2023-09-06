cdcf0f0c86e0352db581862fa878e94bc659de01
Jacob Shulenberger <jacob@builtdigital.io>
2023-09-06 13:39:57 -0500

    feat: add forestfox colors to everforest setup

--- nvim/after/plugin/everforest.lua
+++ nvim/after/plugin/everforest.lua
@@ -1,3 +1,36 @@
 require("everforest").setup({
-  background = "medium"
+  background = "medium",
+  colours_override = function (palette)
+    palette.bg_dim       = "#232b31"
+    palette.bg0          = "#283038"
+    palette.bg1          = "#2e373f"
+    palette.bg2          = "#374149"
+    palette.bg3          = "#404b54"
+    palette.bg4          = "#47525c"
+    palette.bg5          = "#526564"
+
+    palette.bg_red       = "#513c40"
+    palette.bg_visual    = "#563945"
+    palette.bg_blue      = "#374b5f"
+    palette.bg_green     = "#3c4c44"
+    palette.bg_yellow    = "#4f4f41"
+   
+    palette.fg           = "#d0c9a1"
+    palette.red          = "#da7280"
+    palette.orange       = "#eda173"
+    palette.yellow       = "#dbba79"
+    palette.green        = "#a8c180"
+    palette.aqua         = "#7bc29a"
+    palette.blue         = "#78b1bd"
+    palette.purple       = "#d491b5"
+   
+    palette.grey0        = "#738478"
+    palette.grey1        = "#7f928d"
+    palette.grey2        = "#95a8a2"    
+   
+    palette.statusline1  = "#7eb365"
+    palette.statusline2  = "#788492"
+    palette.statusline3  = "#e68274"
+  end
 })
+
