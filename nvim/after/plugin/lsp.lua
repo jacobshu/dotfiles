05a21d72aef5e6fab3022421284f9fcfcb414c99
Jacob Shulenberger <jacob@builtdigital.io>
2023-10-11 12:09:57 -0500

    fix: update lsp settings to approach peak

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -2,55 +2,85 @@ local lsp = require("lsp-zero")
 
 lsp.preset("recommended")
 
-lsp.ensure_installed({
-  'tsserver',
-  'rust_analyzer',
-})
-
--- Fix Undefined global 'vim'
-lsp.nvim_workspace()
-
-
 local cmp = require('cmp')
-local cmp_select = {behavior = cmp.SelectBehavior.Select}
-local cmp_mappings = lsp.defaults.cmp_mappings({
-  ['<S-Tab>'] = cmp.mapping.select_prev_item(cmp_select),
-  ['<Tab>'] = cmp.mapping.select_next_item(cmp_select),
-  ['<CR>'] = cmp.mapping.confirm({ select = true }),
-  ["<C-Space>"] = cmp.mapping.complete(),
-})
-
---cmp_mappings['<Tab>'] = nil
---cmp_mappings['<S-Tab>'] = nil
-
-lsp.setup_nvim_cmp({
-  mapping = cmp_mappings
+local cmp_select = { behavior = cmp.SelectBehavior.Select }
+cmp.setup({
+  mapping = cmp.mapping.preset.insert({
+    ['<S-Tab>'] = cmp.mapping.select_prev_item(cmp_select),
+    ['<Tab>'] = cmp.mapping.select_next_item(cmp_select),
+    ['<CR>'] = cmp.mapping.confirm({ select = true }),
+    ["<C-Space>"] = cmp.mapping.complete(),
+  })
 })
 
 lsp.set_preferences({
     suggest_lsp_servers = false,
     sign_icons = {
-        error = 'E',
-        warn = 'W',
-        hint = 'H',
-        info = 'I'
+      error = '✘',
+      warn = '▲',
+      hint = '⚑',
+      info = '»',
     }
 })
 
 lsp.on_attach(function(client, bufnr)
-  local opts = {buffer = bufnr, remap = false}
-
-  vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-  vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
-  vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
-  vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
-  vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
-  vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
-  vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
-  vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
-  vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
-  vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+  lsp.default_keymaps({buffer = bufnr})
 end)
+
+local lua_opts = lsp.nvim_lua_ls()
+
+require('mason').setup({})
+require('mason-lspconfig').setup({
+  ensure_installed = {
+    'cssls',
+    'gopls',
+    'html',
+    'intelephense',
+    'lua_ls',
+    'tsserver',
+    'rust_analyzer',
+  },
+  handlers = {
+    lsp.default_setup,
+    lua_ls = function()
+      local lua_opts = lsp_zero.nvim_lua_ls()
+      require('lspconfig').lua_ls.setup(lua_opts)
+    end,
+    intelephense = function()
+      require('lspconfig').intelephense.setup({
+        -- Add wordpress to the list of stubs
+        stubs = {
+            "apache", "bcmath", "bz2", "calendar", "com_dotnet", "Core", "ctype", "curl", "date",
+            "dba", "dom", "enchant", "exif", "FFI", "fileinfo", "filter", "fpm", "ftp", "gd", "gettext",
+            "gmp", "hash", "iconv", "imap", "intl", "json", "ldap", "libxml", "mbstring", "meta", "mysqli",
+            "oci8", "odbc", "openssl", "pcntl", "pcre", "PDO", "pdo_ibm", "pdo_mysql", "pdo_pgsql", "pdo_sqlite", "pgsql",
+            "Phar", "posix", "pspell", "readline", "Reflection", "session", "shmop", "SimpleXML", "snmp", "soap",
+            "sockets", "sodium", "SPL", "sqlite3", "standard", "superglobals", "sysvmsg", "sysvsem", "sysvshm", "tidy",
+            "tokenizer", "xml", "xmlreader", "xmlrpc", "xmlwriter", "xsl", "Zend OPcache", "zip", "zlib",
+            "wordpress", "phpunit",
+        },
+      }) 
+    end,
+  }
+})
+
+
+lsp.format_on_save({
+  format_opts = {
+    async = false,
+    timeout_ms = 10000,
+  },
+  servers = {
+    ['tsserver'] = {'javascript', 'typescript'},
+    ['rust_analyzer'] = {'rust'},
+  }
+})
+
+require('flutter-tools').setup({
+  lsp = {
+    capabilities = lsp.get_capabilities()
+  }
+})
 
 lsp.setup()
 
--- nvim/lua/jacobshu/lazy.lua
+++ nvim/lua/jacobshu/lazy.lua
@@ -1,51 +1,65 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+    vim.fn.system(
+        {
+            "git",
+            "clone",
+            "--filter=blob:none",
+            "https://github.com/folke/lazy.nvim.git",
+            "--branch=stable", -- latest stable release
+            lazypath
+        }
+    )
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup({
-  "EdenEast/nightfox.nvim", 
-	{
-    "neanias/everforest-nvim",
-    version = false,
-    lazy = false,
-    priority = 1000, -- make sure to load this before all the other start plugins
-  },
-	{
-		'nvim-telescope/telescope.nvim', tag = '0.1.1',
-		dependencies = { 'nvim-lua/plenary.nvim' }
-	},
-	{ "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
-	'nvim-treesitter/playground',
-	'theprimeagen/harpoon',
-	'mbbill/undotree',
-	'tpope/vim-fugitive',
-	{
-		'VonHeikemen/lsp-zero.nvim',
-		branch = 'v2.x',
-		dependencies = {
-			-- LSP Support
-			{'neovim/nvim-lspconfig'},             -- Required
-			{                                      -- Optional
-			'williamboman/mason.nvim',
-			build = function()
-				pcall(vim.cmd, 'MasonUpdate')
-			end,
-		},
-		{'williamboman/mason-lspconfig.nvim'}, -- Optional
-
-		-- Autocompletion
-		{'hrsh7th/nvim-cmp'},     -- Required
-		{'hrsh7th/cmp-nvim-lsp'}, -- Required
-		{'L3MON4D3/LuaSnip'},     -- Required
-	}
-}
-})
+require("lazy").setup(
+    {
+        "EdenEast/nightfox.nvim",
+        {
+            "neanias/everforest-nvim",
+            version = false,
+            lazy = false,
+            priority = 1000 -- make sure to load this before all the other start plugins
+        },
+        {
+            "nvim-telescope/telescope.nvim",
+            tag = "0.1.1",
+            dependencies = {"nvim-lua/plenary.nvim"}
+        },
+        { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
+        "nvim-treesitter/playground",
+        "theprimeagen/harpoon",
+        "mbbill/undotree",
+        "tpope/vim-fugitive",
+        {
+            "VonHeikemen/lsp-zero.nvim",
+            branch = "v3.x",
+            dependencies = {
+                -- LSP Support
+                {"neovim/nvim-lspconfig"}, -- Required
+                {
+                    -- Optional
+                    "williamboman/mason.nvim",
+                    build = function()
+                        pcall(vim.cmd, "MasonUpdate")
+                    end
+                },
+                {"williamboman/mason-lspconfig.nvim"}, -- Optional
+                -- Autocompletion
+                {"hrsh7th/nvim-cmp"}, -- Required
+                {"hrsh7th/cmp-nvim-lsp"}, -- Required
+                {"L3MON4D3/LuaSnip"} -- Required
+            }
+        },
+        {
+          'akinsho/flutter-tools.nvim',
+          lazy = false,
+          dependencies = {
+              'nvim-lua/plenary.nvim',
+              'stevearc/dressing.nvim', -- optional for vim.ui.select
+          },
+          config = true,
+        }
+    }
+)
