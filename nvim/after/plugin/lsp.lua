509b3565006de2142aed5b9b71ccb4215efa516d
Jacob Shulenberger <jacob@builtdigital.io>
2023-10-17 10:41:57 -0500

    fix: variable scoping in lsp config

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -43,7 +43,7 @@ require('mason-lspconfig').setup({
   handlers = {
     lsp.default_setup,
     lua_ls = function()
-      local lua_opts = lsp_zero.nvim_lua_ls()
+      local lua_opts = lsp.nvim_lua_ls()
       require('lspconfig').lua_ls.setup(lua_opts)
     end,
     intelephense = function()
