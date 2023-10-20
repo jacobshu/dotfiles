66f8bc208cce6288ec8e31b40dc6dbef39650307
Jacob Shulenberger <jacob@builtdigital.io>
2023-10-20 11:44:35 -0500

    chore: formatting lua/jacobshu

--- nvim/lua/jacobshu/lazy.lua
+++ nvim/lua/jacobshu/lazy.lua
@@ -1,64 +1,64 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-    vim.fn.system(
-        {
-            "git",
-            "clone",
-            "--filter=blob:none",
-            "https://github.com/folke/lazy.nvim.git",
-            "--branch=stable", -- latest stable release
-            lazypath
-        }
-    )
+  vim.fn.system(
+    {
+      "git",
+      "clone",
+      "--filter=blob:none",
+      "https://github.com/folke/lazy.nvim.git",
+      "--branch=stable",       -- latest stable release
+      lazypath
+    }
+  )
 end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup(
+  {
+    "EdenEast/nightfox.nvim",
     {
-        "EdenEast/nightfox.nvim",
+      "neanias/everforest-nvim",
+      version = false,
+      lazy = false,
+      priority = 1000       -- make sure to load this before all the other start plugins
+    },
+    {
+      "nvim-telescope/telescope.nvim",
+      dependencies = { "nvim-lua/plenary.nvim" }
+    },
+    { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
+    "nvim-treesitter/playground",
+    "theprimeagen/harpoon",
+    "mbbill/undotree",
+    "tpope/vim-fugitive",
+    {
+      "VonHeikemen/lsp-zero.nvim",
+      branch = "v3.x",
+      dependencies = {
+        -- LSP Support
+        { "neovim/nvim-lspconfig" },       -- Required
         {
-            "neanias/everforest-nvim",
-            version = false,
-            lazy = false,
-            priority = 1000 -- make sure to load this before all the other start plugins
+          -- Optional
+          "williamboman/mason.nvim",
+          build = function()
+            pcall(vim.cmd, "MasonUpdate")
+          end
         },
-        {
-            "nvim-telescope/telescope.nvim",
-            dependencies = {"nvim-lua/plenary.nvim"}
-        },
-        { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
-        "nvim-treesitter/playground",
-        "theprimeagen/harpoon",
-        "mbbill/undotree",
-        "tpope/vim-fugitive",
-        {
-            "VonHeikemen/lsp-zero.nvim",
-            branch = "v3.x",
-            dependencies = {
-                -- LSP Support
-                {"neovim/nvim-lspconfig"}, -- Required
-                {
-                    -- Optional
-                    "williamboman/mason.nvim",
-                    build = function()
-                        pcall(vim.cmd, "MasonUpdate")
-                    end
-                },
-                {"williamboman/mason-lspconfig.nvim"}, -- Optional
-                -- Autocompletion
-                {"hrsh7th/nvim-cmp"}, -- Required
-                {"hrsh7th/cmp-nvim-lsp"}, -- Required
-                {"L3MON4D3/LuaSnip"} -- Required
-            }
-        },
-        {
-          'akinsho/flutter-tools.nvim',
-          lazy = false,
-          dependencies = {
-              'nvim-lua/plenary.nvim',
-              'stevearc/dressing.nvim', -- optional for vim.ui.select
-          },
-          config = true,
-        }
+        { "williamboman/mason-lspconfig.nvim" },       -- Optional
+        -- Autocompletion
+        { "hrsh7th/nvim-cmp" },                        -- Required
+        { "hrsh7th/cmp-nvim-lsp" },                    -- Required
+        { "L3MON4D3/LuaSnip" }                         -- Required
+      }
+    },
+    {
+      'akinsho/flutter-tools.nvim',
+      lazy = false,
+      dependencies = {
+        'nvim-lua/plenary.nvim',
+        'stevearc/dressing.nvim',       -- optional for vim.ui.select
+      },
+      config = true,
     }
+  }
 )
--- nvim/lua/jacobshu/remap.lua
+++ nvim/lua/jacobshu/remap.lua
@@ -14,10 +14,10 @@ vim.keymap.set("n", "N", "Nzzzv")
 vim.keymap.set("x", "<leader>p", [["_dP]])
 
 -- next greatest remap ever : asbjornHaland
-vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
 
-vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
+vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
 
 vim.keymap.set("n", "Q", "<nop>")
 vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
@@ -34,6 +34,5 @@ vim.keymap.set("n", "<leader>vpp", "<cmd>e ~/.dotfiles/nvim/.config/nvim/lua/the
 vim.keymap.set("n", "<leader>mr", "<cmd>CellularAutomaton make_it_rain<CR>");
 
 vim.keymap.set("n", "<leader><leader>", function()
-    vim.cmd("so")
+  vim.cmd("so")
 end)
-
