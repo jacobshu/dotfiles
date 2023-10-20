7b1158b5bc66b0ed449b753530f4aad4e5a57f9b
Jacob Shulenberger <jacob@builtdigital.io>
2023-10-20 10:37:56 -0500

    chore/feat: lint plugin/, add req lsp/treesitter

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -2,30 +2,30 @@ local lsp = require("lsp-zero")
 
 lsp.preset("recommended")
 
-local cmp = require('cmp')
+local cmp = require("cmp")
 local cmp_select = { behavior = cmp.SelectBehavior.Select }
 cmp.setup({
   mapping = cmp.mapping.preset.insert({
-    ['<S-Tab>'] = cmp.mapping.select_prev_item(cmp_select),
-    ['<Tab>'] = cmp.mapping.select_next_item(cmp_select),
-    ['<CR>'] = cmp.mapping.confirm({ select = true }),
+    ["<S-Tab>"] = cmp.mapping.select_prev_item(cmp_select),
+    ["<Tab>"] = cmp.mapping.select_next_item(cmp_select),
+    ["<CR>"] = cmp.mapping.confirm({ select = true }),
     ["<C-Space>"] = cmp.mapping.complete(),
   })
 })
 
 lsp.set_preferences({
-    suggest_lsp_servers = false,
-    sign_icons = {
-      error = '✘',
-      warn = '▲',
-      hint = '⚑',
-      info = '»',
-    }
+  suggest_lsp_servers = false,
+  sign_icons = {
+    error = "✘",
+    warn = "▲",
+    hint = "⚑",
+    info = "»",
+  }
 })
 
 lsp.on_attach(function(client, bufnr)
-  lsp.default_keymaps({buffer = bufnr})
-  local opts = {buffer = bufnr, remap = false}
+  lsp.default_keymaps({ buffer = bufnr })
+  local opts = { buffer = bufnr, remap = false }
   vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
   vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
   vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
@@ -38,39 +38,46 @@ lsp.on_attach(function(client, bufnr)
   vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
 end)
 
-local lua_opts = lsp.nvim_lua_ls()
-
-require('mason').setup({})
-require('mason-lspconfig').setup({
+require("mason").setup({})
+require("mason-lspconfig").setup({
   ensure_installed = {
-    'cssls',
-    'gopls',
-    'html',
-    'intelephense',
-    'lua_ls',
-    'tsserver',
-    'rust_analyzer',
+    "astro",
+    "cssls",
+    "gopls",
+    "html",
+    "intelephense",
+    "lua_ls",
+    "mdx_analyzer",
+    "omnisharp",
+    "rust_analyzer",
+    "sqlls",
+    "svelte",
+    "tsserver",
+    "volar",
+    --"gofumpt",
+    --"prettier",
+    --"stylua",
   },
   handlers = {
     lsp.default_setup,
     lua_ls = function()
       local lua_opts = lsp.nvim_lua_ls()
-      require('lspconfig').lua_ls.setup(lua_opts)
+      require("lspconfig").lua_ls.setup(lua_opts)
     end,
     intelephense = function()
-      require('lspconfig').intelephense.setup({
+      require("lspconfig").intelephense.setup({
         -- Add wordpress to the list of stubs
         stubs = {
-            "apache", "bcmath", "bz2", "calendar", "com_dotnet", "Core", "ctype", "curl", "date",
-            "dba", "dom", "enchant", "exif", "FFI", "fileinfo", "filter", "fpm", "ftp", "gd", "gettext",
-            "gmp", "hash", "iconv", "imap", "intl", "json", "ldap", "libxml", "mbstring", "meta", "mysqli",
-            "oci8", "odbc", "openssl", "pcntl", "pcre", "PDO", "pdo_ibm", "pdo_mysql", "pdo_pgsql", "pdo_sqlite", "pgsql",
-            "Phar", "posix", "pspell", "readline", "Reflection", "session", "shmop", "SimpleXML", "snmp", "soap",
-            "sockets", "sodium", "SPL", "sqlite3", "standard", "superglobals", "sysvmsg", "sysvsem", "sysvshm", "tidy",
-            "tokenizer", "xml", "xmlreader", "xmlrpc", "xmlwriter", "xsl", "Zend OPcache", "zip", "zlib",
-            "wordpress", "phpunit",
+          "apache", "bcmath", "bz2", "calendar", "com_dotnet", "Core", "ctype", "curl", "date",
+          "dba", "dom", "enchant", "exif", "FFI", "fileinfo", "filter", "fpm", "ftp", "gd", "gettext",
+          "gmp", "hash", "iconv", "imap", "intl", "json", "ldap", "libxml", "mbstring", "meta", "mysqli",
+          "oci8", "odbc", "openssl", "pcntl", "pcre", "PDO", "pdo_ibm", "pdo_mysql", "pdo_pgsql", "pdo_sqlite", "pgsql",
+          "Phar", "posix", "pspell", "readline", "Reflection", "session", "shmop", "SimpleXML", "snmp", "soap",
+          "sockets", "sodium", "SPL", "sqlite3", "standard", "superglobals", "sysvmsg", "sysvsem", "sysvshm", "tidy",
+          "tokenizer", "xml", "xmlreader", "xmlrpc", "xmlwriter", "xsl", "Zend OPcache", "zip", "zlib",
+          "wordpress", "phpunit",
         },
-      }) 
+      })
     end,
   }
 })
@@ -82,12 +89,12 @@ lsp.format_on_save({
     timeout_ms = 10000,
   },
   servers = {
-    ['tsserver'] = {'javascript', 'typescript'},
-    ['rust_analyzer'] = {'rust'},
+    ["tsserver"] = { "javascript", "typescript" },
+    ["rust_analyzer"] = { "rust" },
   }
 })
 
-require('flutter-tools').setup({
+require("flutter-tools").setup({
   lsp = {
     capabilities = lsp.get_capabilities()
   }
@@ -96,5 +103,5 @@ require('flutter-tools').setup({
 lsp.setup()
 
 vim.diagnostic.config({
-    virtual_text = true
+  virtual_text = true
 })
--- nvim/after/plugin/treesitter.lua
+++ nvim/after/plugin/treesitter.lua
@@ -1,6 +1,25 @@
-require'nvim-treesitter.configs'.setup {
-  -- A list of parser names, or "all" (the five listed parsers should always be installed)
-  ensure_installed = { "javascript", "typescript", "rust", "markdown", "c", "lua", "vim", "vimdoc", },
+require "nvim-treesitter.configs".setup {
+  ensure_installed = {
+    "astro",
+    "c",
+    "css",
+    "dart",
+    "go",
+    "javascript",
+    "json",
+    "lua",
+    "markdown",
+    "php",
+    "rust",
+    "sql",
+    "svelte",
+    "toml",
+    "typescript",
+    "typescript",
+    "vim",
+    "vimdoc",
+    "vue",
+  },
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
@@ -13,7 +32,7 @@ require'nvim-treesitter.configs'.setup {
     enable = true,
 
     -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+    -- Set this to `true` if you depend on "syntax" being enabled (like for indentation).
     -- Using this option may slow down your editor, and you may see some duplicate highlights.
     -- Instead of true it can also be a list of languages
     additional_vim_regex_highlighting = false,
