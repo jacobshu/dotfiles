84dc91cf620f1cb9fb562d945911cb0da9ced3f1
Jacob Shulenberger <jacob@builtdigital.io>
2023-10-20 13:18:33 -0500

    chore: add telescope file browser for trial

--- nvim/lua/jacobshu/init.lua
+++ nvim/lua/jacobshu/init.lua
@@ -3,4 +3,3 @@ require("jacobshu.lazy")
 require("jacobshu.set")
 
 vim.cmd("colorscheme everforest")
-
--- nvim/lua/jacobshu/lazy.lua
+++ nvim/lua/jacobshu/lazy.lua
@@ -6,7 +6,7 @@ if not vim.loop.fs_stat(lazypath) then
       "clone",
       "--filter=blob:none",
       "https://github.com/folke/lazy.nvim.git",
-      "--branch=stable",       -- latest stable release
+      "--branch=stable", -- latest stable release
       lazypath
     }
   )
@@ -20,11 +20,15 @@ require("lazy").setup(
       "neanias/everforest-nvim",
       version = false,
       lazy = false,
-      priority = 1000       -- make sure to load this before all the other start plugins
+      priority = 1000 -- make sure to load this before all the other start plugins
     },
     {
       "nvim-telescope/telescope.nvim",
       dependencies = { "nvim-lua/plenary.nvim" }
+    },
+    {
+      "nvim-telescope/telescope-file-browser.nvim",
+      dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim", "kyazdani42/nvim-web-devicons" }
     },
     { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
     "nvim-treesitter/playground",
@@ -36,7 +40,7 @@ require("lazy").setup(
       branch = "v3.x",
       dependencies = {
         -- LSP Support
-        { "neovim/nvim-lspconfig" },       -- Required
+        { "neovim/nvim-lspconfig" }, -- Required
         {
           -- Optional
           "williamboman/mason.nvim",
@@ -44,11 +48,11 @@ require("lazy").setup(
             pcall(vim.cmd, "MasonUpdate")
           end
         },
-        { "williamboman/mason-lspconfig.nvim" },       -- Optional
+        { "williamboman/mason-lspconfig.nvim" }, -- Optional
         -- Autocompletion
-        { "hrsh7th/nvim-cmp" },                        -- Required
-        { "hrsh7th/cmp-nvim-lsp" },                    -- Required
-        { "L3MON4D3/LuaSnip" }                         -- Required
+        { "hrsh7th/nvim-cmp" },                  -- Required
+        { "hrsh7th/cmp-nvim-lsp" },              -- Required
+        { "L3MON4D3/LuaSnip" }                   -- Required
       }
     },
     {
@@ -56,7 +60,7 @@ require("lazy").setup(
       lazy = false,
       dependencies = {
         'nvim-lua/plenary.nvim',
-        'stevearc/dressing.nvim',       -- optional for vim.ui.select
+        'stevearc/dressing.nvim', -- optional for vim.ui.select
       },
       config = true,
     }
