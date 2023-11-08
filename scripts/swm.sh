143a540c6b93c454242f43039069bcc8c314784b
Jacob Shulenberger <jacob@builtdigital.io>
2023-11-07 18:09:05 -0600

    feat: tmux swm minimal config

--- scripts/swm.sh
+++ scripts/swm.sh
@@ -2,6 +2,12 @@
 
 session="swm"
 session_exists=$(tmux list-sessions | grep $session)
+window=${session}:"dev-procs"
+docker=${window}.0
+api=${window}.1
+ngrok=${window}.2
+vue=${window}.3
+project_dir="/Users/jacobshu/dev/bn-modern"
 
 if [[ -z $session_exists ]]; then
   # start session with name and detach
@@ -9,27 +15,19 @@ if [[ -z $session_exists ]]; then
 
   # name window and start zsh
   tmux rename-window -t 0 "nvim"
-  tmux send-keys -t "nvim" "cd /Users/jacobshu/dev/bn-modern/shiprec && nvim ." Enter
 
-  window="dev-procs"
-  # create new window for background processes and setup pane for docker
+  # create new window for background processes and setup panes
   tmux new-window -t $session:1 -n "dev-procs"
-  tmux send-keys -t $window "echo 'spin up docker...'" Enter
+  tmux split-window -hf -t $docker
+  tmux split-window -v -t $api
+  tmux split-window -v -t $docker
 
-  # setup .net API pane
-  tmux split-window -hf -t $session:1.0
-  tmux send-keys -t $window "cd /Users/jacobshu/dev/bn-modern/API && echo 'Run the API'" Enter
-
-
-  # setup ngrok pane
-  tmux split-window -v -t $session:1.1
-  tmux send-keys -t $window "echo ngrokn" Enter
-
-  # setup Vue pane
-  tmux split-window -v -t $session:1.0
-  tmux send-keys -t $window "echo 'take in the vue'" Enter
-
-  fi
+  tmux send-keys -t "nvim" "cd $project_dir/shiprec && nvim ." Enter
+  tmux send-keys -t $docker "docker run swmdb" Enter
+  tmux send-keys -t $api "cd $project_dir/API" Enter "dotnet run"
+  tmux send-keys -t $ngrok "ngrok http localhost:5003" Enter
+  tmux send-keys -t $vue "cd $projcect_dir/shiprec" Enter "pnpm run dev" Enter
+fi
 
 # Attach Session, on the Main window
 tmux attach-session -t $session:0
