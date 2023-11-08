c1791ec42e17d1f63960fdd961ce2a85504bdf59
Jacob Shulenberger <jacob@builtdigital.io>
2023-11-08 13:04:00 -0600

    feat: add nvim-surround plugin

--- nvim/lua/jacobshu/lazy.lua
+++ nvim/lua/jacobshu/lazy.lua
@@ -36,6 +36,14 @@ require("lazy").setup(
     "mbbill/undotree",
     "tpope/vim-fugitive",
     {
+      "kylechui/nvim-surround",
+      version = "*", -- Use for stability; omit to use `main` branch for the latest features
+      event = "VeryLazy",
+      config = function()
+          require("nvim-surround").setup({})
+      end
+    },
+    {
       'numToStr/Comment.nvim',
       opts = {
         -- add any options here
