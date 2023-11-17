f8e6b2cee07c03f1314bc6da0e4d18382164a5da
Jacob Shulenberger <jacob@builtdigital.io>
2023-11-17 07:20:31 -0600

    wip: update startup script

--- scripts/swm.sh
+++ scripts/swm.sh
@@ -1,5 +1,30 @@
 #!/usr/bin/env zsh
 
+# check if docker is installed
+if ! command -v docker &> /dev/null
+then
+  echo "docker could not be found"
+  echo "download and install docker from https://docs.docker.com/engine/install/"
+  exit
+fi
+
+# check if ngrok is installed
+if ! command -v ngrok &> /dev/null
+then
+  echo "ngrok could not be found"
+  echo "download and install ngrok from https://ngrok.com/download"
+  exit
+fi
+
+# check if dotnet is installed
+if ! command -v dotnet &> /dev/null
+then
+  echo "dotnet could not be found"
+  echo "download and install dotnet from https://dotnet.microsoft.com/download"
+  exit
+fi
+
+
 session="swm"
 session_exists=$(tmux list-sessions | grep $session)
 window=${session}:"dev-procs"
@@ -7,20 +32,29 @@ docker=${window}.0
 api=${window}.1
 ngrok=${window}.2
 vue=${window}.3
-project_dir="/Users/jacobshu/dev/bn-modern"
+project_dir=""
+
 
 if [[ -z $session_exists ]]; then
   # start session with name and detach
   tmux new-session -d -s $session
 
   # name window and start zsh
-  tmux rename-window -t 0 "nvim"
-
-  # create new window for background processes and setup panes
-  tmux new-window -t $session:1 -n "dev-procs"
+  tmux rename-window -t 0 "dev-procs"
   tmux split-window -vf -t $docker
   tmux split-window -v -t $api
   tmux split-window -v -t $docker
+
+  # create new window for background processes and setup panes
+  tmux new-window -t $session:1 -n "nvim"
+
+  if [[ $1 == "-e" ]]; then
+    tmux rename-window -t 0 "nvim"
+    tmux send-keys -t "nvim" "cd ${project_dir}/shiprec && nvim ." Enter
+    tmux attach-session -t $session:0
+    exit
+  fi
+
 
   tmux send-keys -t "nvim" "cd ${project_dir}/shiprec && nvim ." Enter
   tmux send-keys -t $docker "docker start --attach swm" Enter
