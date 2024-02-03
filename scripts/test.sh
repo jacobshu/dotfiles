2966ad9806f99f2121454dd97262fe8e8ea95eb5
Jacob Shulenberger <jacob@builtdigital.io>
2024-02-03 12:44:38 -0600

    wip: node generic startup script

--- scripts/test.sh
+++ scripts/test.sh
@@ -2,13 +2,37 @@
 
 cd /Users/jacobshu/dev/asn/client
 
-deps=$(pnpm outdated --json)
-echo $deps
+typeofvar () {
+    local type_signature=$(declare -p "$1" 2>/dev/null)
+
+    if [[ "$type_signature" =~ "declare --" ]]; then
+        printf "string\n"
+    elif [[ "$type_signature" =~ "declare -a" ]]; then
+        printf "array\n"
+    elif [[ "$type_signature" =~ "declare -A" ]]; then
+        printf "map\n"
+    else
+        printf "none\n"
+    fi
+}
+
+pnpm=$(pnpm outdated --json)
 
 # parse the json to a csv
-names=$(echo $deps | jq -r 'keys[]')
-parse=$(echo $deps | jq -r '.[] | [.current, .latest, .wanted] | @csv')
-echo $names
-echo $parse
+n=$(echo $pnpm | jq -r 'keys[]')
+p=$(echo $pnpm | jq -r '.[] | [.current, .latest, .wanted] | @csv')
 
-exit
+names=($n)
+deps=($p)
+length=${#names[@]}
+rows=()
+
+for (( i=0; i<length; i++)); do
+    name=${names[$i]}
+    dep=${deps[$i]}
+    str=$name,$dep
+    rows+=($str)
+done
+
+csv=$(printf "%s" "${rows[@]}")
+echo $csv | cut -d ',' -f 1
