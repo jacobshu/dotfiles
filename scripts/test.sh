ba4bebe3bdfa29fa5baf7f75663c76db8d1ee6d7
Jacob Shulenberger <jacob@builtdigital.io>
2024-02-05 17:08:50 -0600

    wip: almost done node script

--- scripts/test.sh
+++ scripts/test.sh
@@ -2,41 +2,25 @@
 
 cd /Users/jacobshu/dev/asn/client
 
-typeofvar () {
-    local type_signature=$(declare -p "$1" 2>/dev/null)
-
-    if [[ "$type_signature" =~ "declare --" ]]; then
-        printf "string\n"
-    elif [[ "$type_signature" =~ "declare -a" ]]; then
-        printf "array\n"
-    elif [[ "$type_signature" =~ "declare -A" ]]; then
-        printf "map\n"
-    else
-        printf "none\n"
-    fi
-}
-
-pnpm=$(pnpm outdated --json)
-
-# parse the json to a csv
-n=$(echo $pnpm | jq -r 'keys[]')
-p=$(echo $pnpm | jq -r '.[] | [.current, .latest, .wanted] | @csv')
-
-names=($n)
-deps=($p)
-length=${#names[@]}
-rows=()
-
-for (( i=0; i<length; i++)); do
-    name=${names[$i]}
-    dep=${deps[$i]}
-    str=$name,$dep
-    rows+=($str)
-done
-
-csv=$(printf "%s\\\\n" "${rows[@]}")
-
-echo $csv > /Users/jacobshu/dev/asn/client/pnpm.csv
-typeofvar csv
-cut -d, -f1,2,3,4 <<< $csv
-
+to_latest=$(gum confirm "Update to latest?" --affirmative "Do it" --negative "Abort!" --prompt.foreground 11 --prompt.bold --prompt.align 'left' --selected.background 14 --selected.foreground 232 --selected.bold --unselected.background 0 --unselected.foreground 243)
+# pnpm=$(pnpm outdated --json)
+#
+# n=$(echo $pnpm | jq -r 'keys[]')
+#
+# names=($n)
+# pnpm outdated
+# echo "\n"
+# gum style --padding "1 3" --border double --border-foreground 14 --foreground 11 --bold "Apply updates?"
+# # echo "\n"
+# update=$(gum choose --height 5 --item.foreground 4 --selected.foreground 5 --cursor '› ' "None" "All (to latest)" "Select")
+#
+# if [[ $update == "All (to latest)" ]]; then
+#   pnpm update
+# elif [[ $update == "Select" ]]; then
+#   gum style --foreground 11 --bold "Which packages to update?"
+#   to_update=$(gum choose --no-limit --height 5 --item.foreground 4 --selected.foreground 5 --cursor › ${names[@]})
+#   to_latest=$(gum confirm "Update to latest?" --affirmative "Do it" --negative "Abort!" --prompt.foreground 11 --prompt.bold)
+#   # pnpm up "$to_update"
+# else
+#   gum style --foreground 9 --bold "No updates applied"
+# fi
