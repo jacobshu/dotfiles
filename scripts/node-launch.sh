e8585f851436284ef1903ba072946130531a4b09
Jacob Shulenberger <jacob@builtdigital.io>
2024-02-21 15:34:19 -0600

    fix: node-launch script updating logic

--- scripts/node-launch.sh
+++ scripts/node-launch.sh
@@ -12,20 +12,41 @@ else
   echo "\n"
   gum style --padding "1 3" --border double --border-foreground 14 --foreground 11 --bold "Apply updates?"
 
-  update=$(gum choose --height 5 --item.foreground 4 --selected.foreground 5 --cursor '› ' "None" "All (to latest)" "Select")
+  update=$(gum choose --height 5 --item.foreground 4 --selected.foreground 5 --cursor '› ' "None" "All" "Select")
 
-  if [[ $update == "All (to latest)" ]]; then
-    pnpm update
+  if [[ $update == "All" ]]; then
+    gum confirm "To latest or package-specified?" --affirmative "Latest" --negative "Specified" \
+      --prompt.foreground 11 --prompt.bold --prompt.align 'left' \
+      --selected.background 2 --selected.foreground 232 --selected.bold \
+      --unselected.background 0 --unselected.foreground 243 
+   
+    if [[ $? == 0 ]]; then
+      pnpm up --latest
+      echo "\n"
+      gum style --foreground 10 --bold "All packages updated to latest"
+    else
+      pnpm up
+      echo "\n"
+      gum style --foreground 10 --bold "All packages updated to specified versions"
+    fi
     git add package.json pnpm-lock.yaml && git commit -m "chore: update packages"
   elif [[ $update == "Select" ]]; then
     gum style --foreground 11 --bold "Which packages to update?"
     to_update=$(gum choose --no-limit --height 5 --item.foreground 4 --selected.foreground 5 --cursor '› ' ${names[@]})
-    to_latest=$(gum confirm "Update to latest?" --affirmative "Do it" --negative "Abort!" \
+    gum confirm "To latest or package-specified?" --affirmative "Latest" --negative "Specified" \
       --prompt.foreground 11 --prompt.bold --prompt.align 'left' \
       --selected.background 2 --selected.foreground 232 --selected.bold \
-      --unselected.background 0 --unselected.foreground 243)
-    echo "\n"
-    pnpm up "$to_update"
+      --unselected.background 0 --unselected.foreground 243
+   
+    if [[ $? == 0 ]]; then
+      pnpm up --latest "$to_update"
+      echo "\n"
+      gum style --foreground 10 --bold "Packages: $to_update updated to latest"
+    else
+      pnpm up "$to_update"
+      echo "\n"
+      gum style --foreground 10 --bold "Packages: $to_update updated to specified versions"
+    fi
     git add package.json pnpm-lock.yaml && git commit -m "chore: update packages"
   else
     gum style --foreground 9 --bold "No updates applied"
