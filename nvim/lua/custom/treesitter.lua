b8ccebd94420ebbe1b2c9d98da902e7fe63cf99a
Jacob Shulenberger <jacob@builtdigital.io>
2024-10-15 09:16:04 -0500

    chore: dep update

--- nvim/lua/custom/treesitter.lua
+++ nvim/lua/custom/treesitter.lua
@@ -18,16 +18,12 @@ M.setup = function()
 			"svelte",
 			"toml",
 			"typescript",
-			"typescript",
 			"vim",
 			"vimdoc",
 			"vue",
 		},
 		-- Install parsers synchronously (only applied to `ensure_installed`)
 		sync_install = false,
-
-		-- Automatically install missing parsers when entering buffer
-		-- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
 		auto_install = true,
 
 		highlight = {
